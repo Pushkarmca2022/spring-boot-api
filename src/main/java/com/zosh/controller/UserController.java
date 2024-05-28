856b189dd63c497eda38104b0a2afc32773a3a60
Ruchi agarwal <ruchi.agarwal2005@gmail.com>
2024-05-28 11:26:39 +0530

    curd option

--- src/main/java/com/zosh/controller/UserController.java
+++ src/main/java/com/zosh/controller/UserController.java
@@ -1,16 +1,18 @@
 package com.zosh.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-
-
+import java.util.Map;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 //import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -42,7 +44,7 @@ public class UserController {
 	    public List<User> getAllUsers() {
 	        return userrepostry.findAll();
 	    }
-	 @GetMapping("/{id}")
+	 @GetMapping("user/{id}")
 	    public ResponseEntity<User> getUserById(@PathVariable(value = "id") Integer id) throws Exception {
 		   Optional<User> user = userrepostry.findById(id);
 	        if (user.isPresent()) {
@@ -53,6 +55,59 @@ public class UserController {
 		 
 		
 	    }
+	 
+	 @DeleteMapping("user/{id}")
+	    public Map<String, Boolean> deleteUser(@PathVariable(value = "id") Integer id) throws Exception {
+		 Optional<User> user = userrepostry.findById(id);
+		  if (user.isPresent()) {
+			  userrepostry.deleteById(id);
+		        Map<String, Boolean> response = new HashMap<>();
+		        response.put("deleted", Boolean.TRUE);
+		        return response;
+//	            return ResponseEntity.ok().body(user.get());
+	        } else {
+	            throw new Exception("User not found with id " + id);
+	        }
+
+	      
+	    }
+	 
+	 @PutMapping("user/{id}")
+	 public User updateUser(@PathVariable(value = "id") Integer id,
+	                                           @RequestBody User userDetails)throws Exception {
+		 Optional<User> user = userrepostry.findById(id);
+		 if(user.isEmpty()) {
+			 throw new Exception("User not found with id " + id);
+			 
+		 }
+		 User oldUser=user.get();
+		 if(userDetails.getFirstName()!=null) {
+			 oldUser.setFirstName(userDetails.getFirstName());
+		 }
+		 if(userDetails.getEmail()!=null) {
+			 oldUser.setEmail(userDetails.getEmail());
+		 }
+		   User updateuser=userrepostry.save(oldUser);
+		 
+		 
+		 return updateuser;
+	 }
+	 
+//	 // Update a user
+//	    @PutMapping("/{id}")
+//	    public ResponseEntity<User> updateUser(@PathVariable(value = "id") Integer id,
+//	                                           @RequestBody User userDetails) {
+//	    	 Optional<User> user = userrepostry.findById(id);
+////	    	 User user=new User();     
+//
+//	        user.setEmail(userDetails.getEmail());
+//	        user.setFirstName(userDetails.getFirstName());
+//	        user.setLastName(userDetails.getLastName());
+//	        user.setPassword(userDetails.getPassword());
+//
+//	        final User updatedUser = userrepostry.save(user);
+//	        return ResponseEntity.ok(updatedUser);
+//	    }
 
 //	@GetMapping("/users")
 //	public List<User> getUsers() {
